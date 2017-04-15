e9d79ef3b545e44d4e0a6409323203a493fb00cf
tolva <smur@free.fr>
2017-04-15 22:34:49 +0200

    review travis ci

--- spec/views/expenses/index.html.erb_spec.rb
+++ spec/views/expenses/index.html.erb_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'expenses/index', type: :view do
   before(:each) do
-    #assign(:expenses, [
-    #    FactoryGirl.create(:expense, date: '2015-02-18', amount: 10.00, paid: true),
-    #    FactoryGirl.create(:expense, date: '2015-02-20', amount: 10.00, paid: false)
-    #])
-    FactoryGirl.create(:expense, date: '2015-02-18', amount: 10.00, paid: true)
-    FactoryGirl.create(:expense, date: '2015-02-20', amount: 10.00, paid: false)
-    #assign(:expenses, FactoryGirl.create(:expense, date: '2015-02-18', amount: 10.00, paid: true) )
-    #expense = FactoryGirl.create(:expense, date: '2015-02-20', amount: 10.00, paid: false)
+    assign(:incomes, Kaminari.paginate_array([
+         FactoryGirl.create(:expense, date: '2015-02-18', amount: 10.00, received: true),
+         FactoryGirl.create(:expense, date: '2015-02-20', amount: 10.00, received: false)
+     ]).page())
     allow(controller).to receive(:current_user).and_return(Member.new)
   end
 
